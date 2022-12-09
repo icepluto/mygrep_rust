73c6768128d9a18b28611f6ff5b7253ed94f4827
lihao <18792689010@163.com>
2022-12-09 21:50:20 +0800

    TDD

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
-use std::{fs, error::Error};
+use std::{error::Error, fs};
 
-pub fn run(config:Config)->Result<(),Box<dyn Error>>{
-    let content = fs::read_to_string(config.filename)?;
-    println!("{}", content);
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    for line in search(&config.query, &contents){
+        println!("歌词:{}\n",line);
+    }
     Ok(())
 }
 
@@ -11,13 +13,39 @@ pub struct Config {
     pub filename: String,
 }
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config,&'static str> {
-        if args.len()<3{
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
             return Err("not enough arguments");
         }
         let query = args[1].clone();
         let filename = args[2].clone();
 
         Ok(Config { query, filename })
+    }
+}
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut res = Vec::new();
+
+    for line in contents.lines(){
+        if line.contains(query){
+            res.push(line);
+        }
+    }
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+        ";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
     }
 }
