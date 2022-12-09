4702a6d152bc5d9d3fcace3ea3de405db2cf8e26
lihao <18792689010@163.com>
2022-12-09 21:08:08 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use mygrep::Config;
+use std::{env, process};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("problem error :{} ", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = mygrep::run(config) {
+        println!("Application Error : {} ", e);
+        process::exit(1);
+    };
+}
