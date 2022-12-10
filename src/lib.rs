1d76d352457a96f60594a28776ab132d816839a0
lihao <18792689010@163.com>
2022-12-10 17:24:38 +0800

    end

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
-use std::{error::Error, fs};
+use std::{error::Error, fs, env};
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    for line in search(&config.query, &contents){
-        println!("歌词:{}\n",line);
+
+    let res = if config.case_sensitive{
+        search(&config.query, &contents)
+    }else{
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in res {
+        println!("歌词:{}\n", line);
     }
     Ok(())
 }
@@ -11,6 +18,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive:bool,
 }
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
@@ -20,14 +28,28 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Ok(Config { query, filename })
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config { query, filename, case_sensitive })
     }
 }
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut res = Vec::new();
 
+    for line in contents.lines() {
+        if line.contains(query) {
+            res.push(line);
+        }
+    }
+    res
+}
+
+pub fn search_case_insensitive<'a>(query: &str,contents: &'a str)->Vec<&'a str>{
+    let mut res = Vec::new();
+    let query = query.to_lowercase();
+
     for line in contents.lines(){
-        if line.contains(query){
+        if line.to_lowercase().contains(&query){
             res.push(line);
         }
     }
@@ -47,5 +69,17 @@ safe, fast, productive.
 Pick three.
         ";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "RusT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+pick three.
+trust me!
+        ";
+        assert_eq!(vec!["Rust:","trust me!"],search_case_insensitive(query,contents));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("problem error :{} ", err);
+        eprintln!("problem error :{} ", err);
         process::exit(1);
     });
 
     if let Err(e) = mygrep::run(config) {
-        println!("Application Error : {} ", e);
+        eprintln!("Application Error : {} ", e);
         process::exit(1);
     };
 }
