c77ba407e81c567c468cde129478e270c7d0abd7
lihao <18792689010@163.com>
2023-01-11 11:02:57 +0800

    closure iter

--- src/lib.rs
+++ src/lib.rs
@@ -21,12 +21,23 @@ pub struct Config {
     pub case_sensitive:bool,
 }
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        args.next();
+        let query = match args.next(){
+            Some(arg)=>arg,
+            None=>{
+                return Err("没有输入搜索值");
+            }
+        };
+        let filename = match args.next(){
+            Some(arg)=>arg,
+            None=>{
+                return Err("没有输入文件名");
+            }
+        };
 
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
@@ -34,26 +45,12 @@ impl Config {
     }
 }
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut res = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            res.push(line);
-        }
-    }
-    res
+    contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str,contents: &'a str)->Vec<&'a str>{
-    let mut res = Vec::new();
     let query = query.to_lowercase();
-
-    for line in contents.lines(){
-        if line.to_lowercase().contains(&query){
-            res.push(line);
-        }
-    }
-    res
+    contents.lines().filter(|line| line.to_lowercase().contains(&query)).collect()
 }
 
 #[cfg(test)]
